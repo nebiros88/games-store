d150ec29d17c9e2acee9926bdd6eed6ca9890de0
nebiros88 <nebiros1988@gmail.com>
2025-11-26 13:59:18 +0100

    install tailwind css with daisyui, create templates for header and footer

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
+  imports: [Header, Footer],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
-}
+export class App {}
---
+++ client/src/app/components/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../navbar/navbar';
+
+@Component({
+  selector: 'app-header',
+  imports: [Navbar],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {}
