fcdc3e8de5e545ebe2b9c8a31fd0c0eb16580b30
nebiros88 <nebiros1988@gmail.com>
2025-11-27 15:08:30 +0100

    api - enable cross-origin-requests (CORS)

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -7,8 +7,11 @@ var connectionString = builder.Configuration.GetConnectionString("GameStore")
   ?? throw new InvalidOperationException("Connection string 'GameStore' not found.");
 
 builder.Services.AddSqlite<GameStoreContext>(connectionString);
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
